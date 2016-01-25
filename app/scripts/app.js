a838e6f3eaa1e65ea58e98aed985cf30e411ee21
pablohernanaraujo <pablohernanaraujo@gail.com>
2016-01-25 14:23:15 -0300

    1 commit

---
+++ app/scripts/app.js
@@ -0,0 +1,49 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name caribbeanBitchWebApp
+ * @description
+ * # caribbeanBitchWebApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('caribbeanBitchWebApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .run(['$rootScope', function ($rootScope) {
+
+      //create a new instance
+      new WOW().init();
+
+      new WOW().sync();
+
+    $rootScope.$on('$routeChangeStart', function (next, current) {
+
+      //when the view changes sync wow
+      new WOW().sync();
+
+    });
+  }])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/inicio.html',
+        controller: 'InicioCtrl',
+        controllerAs: 'inicio'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/inicio.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name caribbeanBitchWebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the caribbeanBitchWebApp
+ */
+angular.module('caribbeanBitchWebApp')
+  .controller('InicioCtrl', function ($scope, $rootScope) {
+    
+    $rootScope.PAGE = 'inicio';
+
+   	$('.parallax').parallax();
+
+  });
